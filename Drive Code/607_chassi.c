fe0fa7a817334020f2a9365a019fb8d5b91d5c9f
Chris Jacobs <chriskj3@gmail.com>
2014-11-15 10:49:16 -0500

    1st Drive Test
    Added the motor configuration and switched to a tank drive

--- Drive Code/607_chassi.c
+++ Drive Code/607_chassi.c
@@ -1,17 +1,7 @@
 /* Contains all of the robot's base movement code.
 The functions in here will be called by 607_main to move the robot.*/
-void forward(int power){
-
-}
-
-void left(int power){
-
-}
-
-void right(int power){
-
-}
-
-void backwards(int power){
+void drive(int lpower, int rpower){
+		motor[leftDrive]=lpower; //sets the left motor to the power given by the 607_main
+		motor[rightDrive]=rpower; //sets the right motor to the power given by the 607_main
 
 }
