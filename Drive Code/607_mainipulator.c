d94368f217c6578ea268c2157e1a0abed72ee438
Chris Jacobs <chriskj3@gmail.com>
2014-12-08 15:54:22 -0500

    Added the ball capturer and the lift
    Added functions for the ball grabber and the lift

--- Drive Code/607_mainipulator.c
+++ Drive Code/607_mainipulator.c
@@ -1,2 +1,16 @@
 /* Contains all the code for the manipulator. These functions will be called by 607_main
 */
+bool spinning=false;//true if the spinner is spinning
+void lift(int power){ //function to power the lift motors
+	motor[rightLift]=power;
+	motor[leftLift]=power;
+}
+
+void toggleSpinner(){//toggles the spinning boolean
+	spinning=!spinning;
+}
+void spin(){//function to spin the motor that captures balls
+	if(spinning)// if the boolean is true
+		motor[spinner]=100;// then spin the motor
+	else motor[spinner]=0;// otherwise turn the motor off
+}
