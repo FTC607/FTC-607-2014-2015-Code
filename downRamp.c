d7db31e1de7e03d023e5e00bb1f57791803eaf3d
Chris Jacobs <chriskj3@gmail.com>
2015-01-11 12:06:57 -0500

    Some Autonomous
    Started fixing the autonomous

--- downRamp.c
+++ downRamp.c
@@ -1,5 +1,5 @@
-int sleepTime=5000;//time it takes to drive down the ramp
-int power=100; //power to drive the chassi motors
+int sleepTime=3000;//time it takes to drive down the ramp in millseconds
+int power=50; //power to drive the chassi motors
 void downRamp(){
 	 drive(power,power); //runs the motor in 607_chassi
 	Sleep(sleepTime);//sleeps so the motor can remain on
--- scoreBall.c
+++ scoreBall.c
@@ -3,9 +3,9 @@ void scoreBall(){
 	lift(100);//turn the lift on
 	Sleep(sleepTimer); //keep the lift on
 	lift(0); //turn the lift off
-	toggleDoor(); //opens the door
-	sleep(sleepTimer); //waits
-	toggleDoor(); //closes the door
+	//raises arm
+	//waits
+	//drops arm
 	lift(-100); //lower the lift
 	Sleep(sleepTimer); //keep it on
 	lift(0); //turn it off and it is now lowered
